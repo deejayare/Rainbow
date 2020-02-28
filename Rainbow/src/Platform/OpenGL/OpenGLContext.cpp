2e824c203c23f1525f85a8eff0a6fca96d1c2437
deejayare <danroberts407@gmail.com>
2020-02-28 12:56:22 -0800

    Rendered first triangle

--- Rainbow/src/Platform/OpenGL/OpenGLContext.cpp
+++ Rainbow/src/Platform/OpenGL/OpenGLContext.cpp
@@ -18,6 +18,11 @@ namespace Rainbow {
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		RAINBOW_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+		RAINBOW_CORE_INFO("OpenGL Info:");
+		RAINBOW_CORE_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
+		RAINBOW_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
+		RAINBOW_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers()
