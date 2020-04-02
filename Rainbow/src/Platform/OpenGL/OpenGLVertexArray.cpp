1cfeb37a0461a641a7d97f0e3e2125f05d07dffc
deejayare <danroberts407@gmail.com>
2020-04-02 16:31:57 -0700

    Fixed warning in OpenGLVertexArray.cpp and fixed LayerStack bug that occurs if popping an overlay

--- Rainbow/src/Platform/OpenGL/OpenGLVertexArray.cpp
+++ Rainbow/src/Platform/OpenGL/OpenGLVertexArray.cpp
@@ -64,7 +64,7 @@ namespace Rainbow {
 				ShaderDataTypeToOpenGLBaseType(element.Type),
 				element.Normalized ? GL_TRUE : GL_FALSE,
 				layout.GetStride(),
-				(const void*)element.Offset);
+				(const void*)(intptr_t)element.Offset);
 			index++;
 		}
 		m_VertexBuffers.push_back(vertexBuffer);
