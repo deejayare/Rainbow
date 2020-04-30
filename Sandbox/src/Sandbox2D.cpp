5e2489e64a23f4c461e951d5ab97770b32f64cbe
deejayare <danroberts407@gmail.com>
2020-04-30 13:54:09 -0700

    Now uses multiple draw calls if the number of quads exceeds the max per call. Added stats.

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -32,6 +32,7 @@ void Sandbox2D::OnUpdate(Rainbow::Timestep ts)
 
 
 	// Render
+	Rainbow::Renderer2D::ResetStats();
 	{
 		RAINBOW_PROFILE_SCOPE("Renderer Prep");
 		Rainbow::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
@@ -47,8 +48,22 @@ void Sandbox2D::OnUpdate(Rainbow::Timestep ts)
 		Rainbow::Renderer2D::DrawRotatedQuad({ 1.0f, 0.0f }, { 0.8f, 0.8f }, glm::radians(4*rotation) , { 0.8f, 0.2f, 0.3f, 1.0f });
 		Rainbow::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
 		Rainbow::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, m_SquareColor);
-		Rainbow::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
+		Rainbow::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 20.0f, 20.0f }, m_CheckerboardTexture, 10.0f);
 		Rainbow::Renderer2D::DrawRotatedQuad({ -2.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, glm::radians(-rotation), m_CheckerboardTexture, 20.0f);
+		Rainbow::Renderer2D::EndScene();
+
+
+		Rainbow::Renderer2D::BeginScene(m_CameraController.GetCamera());
+		for (float y = -5.0f; y < 5.0f; y += 0.5f)
+		{
+			for (float x = -5.0f; x < 5.0f; x += 0.5f)
+			{
+				glm::vec4 color = { (x + 5.0f) / 10.0f, 0.4f, (y + 5.0f) / 10.0f, 0.8f };
+				//Rainbow::Renderer2D::DrawQuad({ x, y }, { 0.45f, 0.45f }, color);
+				Rainbow::Renderer2D::DrawRotatedQuad({ x, y }, { 0.45f, 0.45f }, glm::radians(rotation * ( 1 - (y + 5.0f) / 10.0f ) ), color);
+			}
+		}
+
 		Rainbow::Renderer2D::EndScene();
 	}
 
@@ -58,6 +73,16 @@ void Sandbox2D::OnImGuiRender()
 {
 	RAINBOW_PROFILE_FUNCTION();
 	ImGui::Begin("Color Settings");
+
+
+	auto stats = Rainbow::Renderer2D::GetStats();
+	ImGui::Text("Renderer2D Stats:");
+	ImGui::Text("Draw Calls: %d", stats.DrawCalls);
+	ImGui::Text("Quads: %d", stats.QuadCount);
+	ImGui::Text("Vertices: %d", stats.GetTotalVertexCount());
+	ImGui::Text("Indicies: %d", stats.GetTotalIndexCount());
+
+
 	ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
 	ImGui::End();
 }
